7373c5c1bc04291f6b376d68672e104022a9f3e8
rickyvr-byte <officialrickyvr@gmail.com>
2026-05-15 21:08:27 +1200

    no clue how to write js so i ai'ed it

--- GTF/script.js
+++ GTF/script.js
@@ -1,11 +1,28 @@
 const panel = document.getElementById("settingsPanel");
 
+function toggleSettings() {
+    panel.classList.toggle("open");
+}
+
+function setColor(variable, color) {
+    document.documentElement.style.setProperty(variable, color);
+    localStorage.setItem(variable, color);
+}
+
+window.onload = () => {
+    ['--bg-color', '--accent-color', '--panel-color'].forEach(variable => {
+        const saved = localStorage.getItem(variable);
+        if (saved) {
+            document.documentElement.style.setProperty(variable, saved);
+        }
+    });
+
+    loadFlags();
+};
+
 async function loadFlags() {
-
     const response = await fetch("flags.json");
-
     flags = await response.json();
-
     nextQuestion();
 }
 
@@ -13,13 +30,15 @@ function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
+let flags = [];
+let currentAnswer = "";
+let locked = false;
+
 function nextQuestion() {
 
     locked = false;
 
-    const randomFlag = flags[
-        Math.floor(Math.random() * flags.length)
-    ];
+    const randomFlag = flags[Math.floor(Math.random() * flags.length)];
 
     currentAnswer = randomFlag.country;
 
@@ -28,11 +47,7 @@ function nextQuestion() {
     let options = [currentAnswer];
 
     while (options.length < 5) {
-
-        const randomCountry = flags[
-            Math.floor(Math.random() * flags.length)
-        ].country;
-
+        const randomCountry = flags[Math.floor(Math.random() * flags.length)].country;
         if (!options.includes(randomCountry)) {
             options.push(randomCountry);
         }
@@ -44,7 +59,7 @@ function nextQuestion() {
 
     buttons.forEach((button, index) => {
 
-        button.innerText = options[index];
+        button.innerText = options[index].replace("Flag of ", "");
 
         button.style.background = "var(--button-color)";
         button.style.filter = "brightness(1)";
@@ -56,8 +71,7 @@ function nextQuestion() {
             locked = true;
 
             buttons.forEach(btn => {
-
-                if (btn.innerText === currentAnswer) {
+                if (btn.innerText === currentAnswer.replace("Flag of ", "")) {
                     btn.style.background = "#42d67a";
                 } else {
                     btn.style.background = "#d94b4b";
@@ -68,7 +82,7 @@ function nextQuestion() {
 
             setTimeout(() => {
                 nextQuestion();
-            }, 5000);
+            }, 2000);
         };
     });
 }
